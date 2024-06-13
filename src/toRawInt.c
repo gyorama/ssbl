aa65a8ec59bd460b68af42f9cef682ba6c7d9eb6
Moldy <lenovoeldotab@gmail.com>
2024-06-13 09:09:31 +0300

    Halve the size of Simple Binary File Maker

--- src/toRawInt.c
+++ src/toRawInt.c
@@ -26,13 +26,13 @@ int main(int argc, const char *argv[]) {
     char *endPtr;
     bool ret;
     char command[200];
-    int magicFileSignature[9] = {0xAF, 0x00, 0xDD, 0xF0,
-                                 0xAA, 0x55, 0xBA, 0xBE, 0x03};
+    int8_t magicFileSignature[9] = {0xAF, 0x00, 0xDD, 0xF0,
+                                    0xAA, 0x55, 0xBA, 0xBE, 0x03};
     enum keywords keyword;
 
     // Write ssbl signature so that the interpreter can't run random binary files
     // What are the chances the 1st 9 bytes in a random file are the exact same as this one anyway
-    fwrite(&magicFileSignature, sizeof(int), 9, target);
+    fwrite(&magicFileSignature, sizeof(int8_t), 9, target);
 
     while (fscanf(source, "%s", command) != EOF) {
 
@@ -44,7 +44,7 @@ int main(int argc, const char *argv[]) {
             val = strtol(valStr, &endPtr, 10);
             
             if (*endPtr == '\0') {
-                fwrite(&keyword, sizeof(int), 1, target);
+                fwrite(&keyword, sizeof(int16_t), 1, target);
                 fwrite(&val, sizeof(int), 1, target);
             } else {
                 puts("Invalid integer");
@@ -52,36 +52,36 @@ int main(int argc, const char *argv[]) {
         // Code gets better from here
         } else if (strcasecmp(command, "pop") == 0) {
             keyword = POP;
-            fwrite(&keyword, sizeof(int), 1, target);
+            fwrite(&keyword, sizeof(int16_t), 1, target);
         } else if (strcasecmp(command, "top") == 0) {
             keyword = TOP;
-            fwrite(&keyword, sizeof(int), 1, target);
+            fwrite(&keyword, sizeof(int16_t), 1, target);
         } else if (strcasecmp(command, "isempty") == 0) {
             keyword = IS_EMPTY;
-            fwrite(&keyword, sizeof(int), 1, target);
+            fwrite(&keyword, sizeof(int16_t), 1, target);
         } else if (strcasecmp(command, "isfull") == 0) {
             keyword = IS_FULL;
-            fwrite(&keyword, sizeof(int), 1, target);
+            fwrite(&keyword, sizeof(int16_t), 1, target);
         } else if (strcasecmp(command, "clear") == 0) {
             keyword = CLEAR;
-            fwrite(&keyword, sizeof(int), 1, target);
+            fwrite(&keyword, sizeof(int16_t), 1, target);
         } else if (strcasecmp(command, "add") == 0) {
             keyword = ADD;
-            fwrite(&keyword, sizeof(int), 1, target);
+            fwrite(&keyword, sizeof(int16_t), 1, target);
         } else if (strcasecmp(command, "sub") == 0) {
             keyword = SUBTRACT;
-            fwrite(&keyword, sizeof(int), 1, target);
+            fwrite(&keyword, sizeof(int16_t), 1, target);
         } else if (strcasecmp(command, "div") == 0) {
             keyword = DIVIDE;
-            fwrite(&keyword, sizeof(int), 1, target);
+            fwrite(&keyword, sizeof(int16_t), 1, target);
         } else if (strcasecmp(command, "mul") == 0) {
             keyword = MULTIPLY;
-            fwrite(&keyword, sizeof(int), 1, target);
+            fwrite(&keyword, sizeof(int16_t), 1, target);
         } else if (strcasecmp(command, "times") == 0) {
             keyword = LOOP;
             fscanf(source, "%s", valStr);
             val = strtol(valStr, &endPtr, 10);
-            fwrite(&keyword, sizeof(int), 1, target);
+            fwrite(&keyword, sizeof(int16_t), 1, target);
             // Make sure this is a valid integer
             if (*endPtr == '\0') {
                 fwrite(&val, sizeof(int), 1, target);
@@ -90,7 +90,7 @@ int main(int argc, const char *argv[]) {
             }
         } else if (strcasecmp(command, "end") == 0) {
             keyword = END;
-            fwrite(&keyword, sizeof(int), 1, target);
+            fwrite(&keyword, sizeof(int16_t), 1, target);
         } else if (strcasecmp(command, "if") == 0) {
             fscanf(source, "%s", valStr);
             if (strcasecmp(valStr, "true") == 0) {
@@ -100,8 +100,8 @@ int main(int argc, const char *argv[]) {
             }
 
             keyword = IF;
-            fwrite(&keyword, sizeof(int), 1, target);
-            fwrite(&val, sizeof(int), 1, target);
+            fwrite(&keyword, sizeof(int16_t), 1, target);
+            fwrite(&val, sizeof(bool), 1, target);
         }
     }
 
