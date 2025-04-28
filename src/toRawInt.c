87926e544e2f7b64a3cf28d611c10668f9c5e8ad
Moldy <Moldy@mx>
2025-04-28 09:18:13 +0300

    Additions

--- src/toRawInt.c
+++ src/toRawInt.c
@@ -19,8 +19,12 @@ int main(int argc, const char *argv[]) {
         perror("Could not open file");
         return 1;
     }
+
+    int16_t varArr[400];
+    memset(varArr, INT16_MIN, sizeof(int16_t));
+
     char command[200];
-    char *cmdPtr = command+1;
+    char *cmdPtr = command;
     char *word = calloc(200, sizeof(char));
     uint8_t magicFileSignature[9] = {0xAF, 0x00, 0xDD, 0xF0,
                                     0xAA, 0x55, 0xBA, 0xBE, 0x03};
@@ -31,10 +35,14 @@ int main(int argc, const char *argv[]) {
     while (fscanf(source, "%s", command) != EOF) {
         if (command[0] == '@') {
             printf("%s: function\n", cmdPtr);
-            memcpy(word, command, strlen(command)+1);
+            memcpy(word, cmdPtr, strlen(command)+1);
         } else if (command[0] == '#') {
             printf("%s: variable\n", cmdPtr);
-            memcpy(word, command, strlen(command)+1);
+            if (atoi(cmdPtr) < 1) {
+                puts("Variable index can't be less than 1\nVariable can't be named with text");
+                return 1;
+            }
+            memcpy(word, cmdPtr, strlen(command)+1);
         } else {
             printf("%s: value to %s\n", cmdPtr, word);
         }
